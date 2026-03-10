25dc1f568424ed2c587d76392ae7d818c6094ab0
elitestayoficial-web <elitestayoficial@gmail.com>
2026-03-10 13:27:16 +0000

    Update main.py

--- main/main.py
+++ main/main.py
@@ -18,3 +18,13 @@ application = app
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+# ... (todo tu código anterior)
+
+# Esto es lo que Gunicorn está buscando
+application = app 
+app = application
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
